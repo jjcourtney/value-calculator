be72a3554abb46297480dc9a7dfe2b809c55e64c
jjcourtney <jcourtneywork@gmail.com>
2026-05-31 17:38:03 +0100

    adds tests

--- test/test_calculations.py
+++ test/test_calculations.py
@@ -1,33 +1,33 @@
-import unittest
-
 from calculations import calculate_result, format_number
 
 
-class TestCalculations(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(calculate_result("Sum", [1.0, 2.0, 3.0]), 6.0)
-
-    def test_average(self):
-        self.assertEqual(calculate_result("Average", [2.0, 4.0, 6.0]), 4.0)
-
-    def test_minimum(self):
-        self.assertEqual(calculate_result("Minimum", [3.0, 1.0, 2.0]), 1.0)
-
-    def test_maximum(self):
-        self.assertEqual(calculate_result("Maximum", [3.0, 1.0, 2.0]), 3.0)
-
-    def test_product(self):
-        self.assertEqual(calculate_result("Product", [2.0, 3.0, 4.0]), 24.0)
-
-    def test_unknown_operation_defaults_to_sum(self):
-        self.assertEqual(calculate_result("Unknown", [1.0, 2.0]), 3.0)
-
-    def test_format_whole_number(self):
-        self.assertEqual(format_number(5.0), "5")
-
-    def test_format_decimal_number(self):
-        self.assertEqual(format_number(2.5), "2.5")
+def test_sum():
+    assert calculate_result("Sum", [1.0, 2.0, 3.0]) == 6.0
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_average():
+    assert calculate_result("Average", [2.0, 4.0, 6.0]) == 4.0
+
+
+def test_minimum():
+    assert calculate_result("Minimum", [3.0, 1.0, 2.0]) == 1.0
+
+
+def test_maximum():
+    assert calculate_result("Maximum", [3.0, 1.0, 2.0]) == 3.0
+
+
+def test_product():
+    assert calculate_result("Product", [2.0, 3.0, 4.0]) == 24.0
+
+
+def test_unknown_operation_defaults_to_sum():
+    assert calculate_result("Unknown", [1.0, 2.0]) == 3.0
+
+
+def test_format_whole_number():
+    assert format_number(5.0) == "5"
+
+
+def test_format_decimal_number():
+    assert format_number(2.5) == "2.5"
